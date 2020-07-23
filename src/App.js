46894939ee62699e094e0124781f55f6b26d04d1
stanislav623 <62516604+stanislav623@users.noreply.github.com>
2020-07-23 11:29:08 -0700

    feat: initial splash screen design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
+        <p></p>
+        <h3>
+          Buying. Selling. Simple.
+        </h3>
+        <p></p>
+        <button type="button" class="btn btn-primary">Sign up</button>
+        <p></p>
+        <p></p>
+        <h6>Already have an account?</h6>
+<button type="button" class="btn btn-secondary">Log in</button>
+<p></p>
+<h6><a
+          className="Btn-main"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Continue as a guest
+        </a></h6>
+        
       </header>
     </div>
   );
