2365fb67091c5b08d381147c065adeb174f8c210
Stanislav Bodurov <stbodurov@icloud.com>
2021-05-11 09:47:03 +0300

    Added Firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export let firebaseConfig = {
+  apiKey: "AIzaSyCtYVtuEyjXm0nbpWEYkrdQ4eTWp-QfumM",
+  authDomain: "shoply-dde6f.firebaseapp.com",
+  databaseURL: "https://shoply-dde6f.firebaseio.com",
+  projectId: "shoply-dde6f",
+  storageBucket: "shoply-dde6f.appspot.com",
+  messagingSenderId: "325515437322",
+  appId: "1:325515437322:web:e47703f26345cdba6886f3",
+  measurementId: "G-RG7WQB2T70"
+}
